7e49141aba1f6bc9b53e34fa37c760097b78060e
bras1223 <l.hermans98@gmail.com>
2017-09-20 12:11:33 +0200

    Initial styling of LoginPage. Looking to change how flex-box arranges the objects.

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,9 +3,12 @@ import { Router as PreactRouter } from "preact-router";
 
 import { history } from "src/support/history";
 import { augmentRouter } from "src/support/pageTransitionSupport";
+import LoginPage from "src/modules/login/containers/LoginPage";
 
 const Router = augmentRouter(PreactRouter);
 
-const App = () => <Router history={history} />;
-
-export default App;
+export default () => (
+  <Router history={history}>
+    <LoginPage path="/login" default />
+  </Router>
+);
--- src/modules/login/containers/LoginPage.js
+++ src/modules/login/containers/LoginPage.js
@@ -1,16 +1,24 @@
 import { h, Component } from "preact";
 import { Page } from "src/components/Page";
-import { Header } from "src/components/Header";
 import I18n from "src/config/i18n";
+
+import styles from "./LoginPage.scss";
 
 export class LoginPage extends Component {
   render() {
     return (
       <Page>
-        <Header>
-          <h1>{I18n.t("login.title")}</h1>
-        </Header>
-        <main />
+        <main class={styles.main}>
+          <div class={styles.logo} />
+          <div class={styles.login}>
+            <button type="button" class={styles.buttonGoogle}>
+              <span>{I18n.t("login.login_using_google")}</span>
+            </button>
+          </div>
+          <div class={styles.disclaimer}>
+            <span>{I18n.t("login.disclaimer")}</span>
+          </div>
+        </main>
       </Page>
     );
   }
