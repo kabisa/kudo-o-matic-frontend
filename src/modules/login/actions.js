61542aa33ba0f8a5d3ff661962959a6f3cc025ad
bras1223 <l.hermans98@gmail.com>
2017-09-25 11:23:27 +0200

    Used constants to define existing action-types.

---
+++ src/modules/login/actions.js
@@ -0,0 +1,45 @@
+import * as constants from "./constants";
+import { requestToken } from "./apiClient";
+
+const handleGoogleLoginSuccess = token => {
+  return {
+    type: constants.GOOGLE_TOKEN_SUCCESS,
+    token: token
+  };
+};
+
+const handleApiLoginSuccess = token => {
+  return {
+    type: constants.API_TOKEN_SUCCESS,
+    token: token
+  };
+};
+
+const handleAPILoginFailure = error => {
+  return {
+    type: constants.API_TOKEN_FAILURE,
+    error: error
+  };
+};
+
+export const handleGoogleLoginFailure = error => {
+  return {
+    type: constants.GOOGLE_TOKEN_FAILURE,
+    error: error
+  };
+};
+
+export const requestApiToken = googleToken => {
+  return dispatch => {
+    dispatch(handleGoogleLoginSuccess(googleToken));
+
+    return requestToken(googleToken).then(
+      token => {
+        return dispatch(handleApiLoginSuccess(token));
+      },
+      error => {
+        return dispatch(handleAPILoginFailure(error));
+      }
+    );
+  };
+};
