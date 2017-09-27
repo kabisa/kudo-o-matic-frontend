fac7e549b2518c0e7d61ec26461e5dca10feb35d
bras1223 <l.hermans98@gmail.com>
2017-09-27 16:06:56 +0200

    Redux wired GoalPage

--- src/modules/goal/containers/GoalPage.js
+++ src/modules/goal/containers/GoalPage.js
@@ -1,19 +1,42 @@
 import { h, Component } from "preact";
+import { connect } from "preact-redux";
+
 import { Page } from "src/components/Page";
 import { Header } from "src/components/Header";
 import I18n from "src/config/i18n";
+import { fetchCurrentGoalState } from "../actions";
 
 export class GoalPage extends Component {
-  render() {
+  componentWillMount() {
+    this.props.fetchData(this.props.apiToken);
+  }
+
+  render({ currentAmount, nextAmount, nextText }) {
     return (
       <Page>
         <Header>
           <h1>{I18n.t("goal.title")}</h1>
         </Header>
-        <main />
+        <main>
+          <span>{currentAmount}</span>
+          <span>{nextAmount}</span>
+          <span>{nextText}</span>
+        </main>
       </Page>
     );
   }
 }
+const mapStateToProps = state => ({
+  apiToken: state.authentication.apiToken,
+  currentAmount: state.goal.currentBalance.currentAmount,
+  nextAmount: state.goal.nextAmount,
+  nextText: state.goal.nextText
+});
 
-export default GoalPage;
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchData: token => dispatch(fetchCurrentGoalState(token))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GoalPage);
