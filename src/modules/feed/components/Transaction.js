5825909feb40b3a2171bfd8f98644dd4db01efb1
bras1223 <l.hermans98@gmail.com>
2017-10-05 11:40:52 +0200

    Added Transaction-component to show incomming transactions.

--- src/modules/feed/components/Transaction.js
+++ src/modules/feed/components/Transaction.js
@@ -1,15 +1,26 @@
 import { h } from "preact";
 import styles from "./Transaction.scss";
+import LikeIcon from "src/assets/icons/transaction/thumbs-up.svg";
 
-export const Transaction = () => {
+export const Transaction = ({ amount, from, to, reason, likes }) => {
   return (
     <div class={styles.transaction}>
       <div class={styles.transactionContent}>
         <div class={styles.transactionValue}>
-          <div class={styles.value}>
-            13 <span class={styles.kudoCurrency}>₭</span>
-          </div>
+          <div class={styles.value}>{amount}</div>
+          <div class={styles.kudoCurrency}>₭</div>
           <div class={styles.divider} />
+        </div>
+        <div class={styles.transactionDescription}>
+          <div class={styles.transactionText}>
+            {from}: {amount} <span class={styles.kudoCurrency}>₭</span> to {to}{" "}
+            for {reason}
+          </div>
+          <div class={styles.transactionTimestamp}>1 day ago</div>
+        </div>
+        <div class={styles.transactionAction}>
+          <img src={LikeIcon} class={styles.thumb} />
+          <div class={styles.likes}>+{likes}</div>
         </div>
       </div>
       <hr class={styles.hr} />
--- src/modules/feed/containers/FeedPage.js
+++ src/modules/feed/containers/FeedPage.js
@@ -2,6 +2,8 @@ import { h, Component } from "preact";
 import { Page } from "src/components/Page";
 import { Header } from "src/components/Header";
 import I18n from "src/config/i18n";
+import styles from "./FeedPage.scss";
+
 import Transaction from "../components/Transaction";
 
 export class FeedPage extends Component {
@@ -12,18 +14,24 @@ export class FeedPage extends Component {
           <h1>{I18n.t("feed.title")}</h1>
         </Header>
         <main />
-        <ul>
+        <ul class={styles.transactionList}>
           <li>
-            <Transaction />
+            <Transaction
+              amount={100}
+              from="Luuk Hermans"
+              to="Robin Laugs"
+              reason="Sprint 1 delivery"
+              likes={1}
+            />
           </li>
           <li>
-            <Transaction />
-          </li>
-          <li>
-            <Transaction />
-          </li>
-          <li>
-            <Transaction />
+            <Transaction
+              amount={5}
+              from="Luuk Hermans"
+              to="Robin Laugs"
+              reason="arranging API agreements"
+              likes={0}
+            />
           </li>
         </ul>
       </Page>
