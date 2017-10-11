a4040017d0bbbb9d183801df33e91ca5a7d287f1
bras1223 <l.hermans98@gmail.com>
2017-10-11 15:46:17 +0200

    Now using cordova Google plugin, so this can't be tested anymore.

--- test/spec/modules/login/components/GoogleButton.spec.js
+++ test/spec/modules/login/components/GoogleButton.spec.js
@@ -1,14 +1,6 @@
 import { h, render } from "preact";
 import GoogleButton from "src/modules/login/components/GoogleButton";
-import GoogleLogin from "react-google-login";
-import styles from "src/modules/login/components/GoogleButton.scss";
-import Settings from "src/config/settings";
 import I18n from "src/config/i18n";
-
-import {
-  requestApiToken,
-  handleGoogleLoginFailure
-} from "src/modules/login/actions";
 
 describe("GoogleButton", function() {
   let scratch, mount;
@@ -18,24 +10,10 @@ describe("GoogleButton", function() {
     mount = jsx => render(jsx, scratch);
   });
 
-  it("shows GoogleLogin Component", function() {
-    const page = mount(
-      <GoogleButton
-        requestApiToken={requestApiToken}
-        handleGoogleLoginFailure={handleGoogleLoginFailure}
-      />
-    );
-    expect(page.outerHTML).to.contain(
-      mount(
-        <GoogleLogin
-          clientId={Settings.googleClientID}
-          buttonText={I18n.t("login.login_using_google")}
-          hostedDomain={Settings.googleHostedDomain}
-          className={styles.buttonGoogle}
-          onSuccess={requestApiToken}
-          onFailure={handleGoogleLoginFailure}
-        />
-      ).outerHTML
+  it("shows GoogleLogin Button", function() {
+    mount(<GoogleButton />);
+    expect(scratch.querySelector("button")).to.have.text(
+      I18n.t("login.login_using_google")
     );
   });
 });
