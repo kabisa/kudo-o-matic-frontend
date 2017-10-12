8c3a8dbf931abf83e6704d09e7d55716c8a984a5
bras1223 <l.hermans98@gmail.com>
2017-10-12 16:54:23 +0200

    API-request to fetch transactions.

---
+++ src/modules/feed/apiClient.js
@@ -0,0 +1,50 @@
+import Settings from "src/config/settings";
+import axios from "axios";
+
+const httpClient = axios.create({
+  baseURL: Settings.apiLocation
+});
+
+export const fetchTransactions = apiToken => {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Api-Token": apiToken
+  };
+
+  return new Promise(resolve => {
+    const request = httpClient.get(
+      "/transactions?include=activity,sender,receiver,votes&sort=-created_at",
+      {
+        headers
+      }
+    );
+
+    request.then(response => {
+      var JSONAPIDeserializer = require("jsonapi-serializer").Deserializer;
+      var TransactionDeserialize = new JSONAPIDeserializer();
+      TransactionDeserialize.deserialize(response.data).then(t => resolve(t));
+    });
+  });
+};
+
+export const voteTransaction = (transactionId, userId, apiToken) => {
+  const headers = {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Api-Token": apiToken
+  };
+
+  return new Promise(resolve => {
+    const request = httpClient.put(
+      "/transactions/" + { transactionId } + "/votes/" + { userId },
+      {
+        headers
+      }
+    );
+
+    request.then(response => {
+      resolve(response.data.data);
+    });
+  });
+};
+
+export const unvoteTransaction = (transactionId, userId, apiToken) => {};
