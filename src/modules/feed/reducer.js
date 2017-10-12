445e9165f0942e778d1dd46db2327ad0b7670039
bras1223 <l.hermans98@gmail.com>
2017-10-12 16:54:43 +0200

    Wrong constants & not returning an initial state.

--- src/modules/feed/reducer.js
+++ src/modules/feed/reducer.js
@@ -8,11 +8,13 @@ const initialState = {
 
 export const feed = (state = initialState, action) => {
   switch (action.type) {
-    case constants.STARTED_FETCHING_GOAL:
+    case constants.STARTED_FETCHING_TRANSACTIONS:
       return { ...state, fetching: true };
     case constants.FINISHED_FETCHING_TRANSACTIONS:
       return { ...state, fetching: false, transactions: action.transactions };
     case constants.RECEIVED_API_ERROR:
       return { ...state, fetching: false, error: action.error };
+    default:
+      return state;
   }
 };
