654c51fa94c339f44aa4be061c0eb143e15c47c2
bras1223 <l.hermans98@gmail.com>
2017-10-17 15:51:35 +0200

    Check whether transaction is liked or not. Change liked value after Like/unLike.

--- src/modules/feed/reducer.js
+++ src/modules/feed/reducer.js
@@ -10,8 +10,50 @@ export const feed = (state = initialState, action) => {
   switch (action.type) {
     case constants.STARTED_FETCHING_TRANSACTIONS:
       return { ...state, fetching: true };
-    case constants.FINISHED_FETCHING_TRANSACTIONS:
-      return { ...state, fetching: false, transactions: action.transactions };
+    case constants.FINISHED_FETCHING_TRANSACTIONS: {
+      const transactions = action.transactions.map(transaction => {
+        let voted = false;
+        transaction.votes.forEach(vote => {
+          if (vote["voter-id"] == action.userId) {
+            voted = true;
+          }
+        });
+        return { ...transaction, voted: voted };
+      });
+      return { ...state, fetching: false, transactions: transactions };
+    }
+    case constants.LIKED_TRANSACTION: {
+      const _transactions = state.transactions.map(
+        transaction =>
+          transaction.id !== action.transactionId
+            ? transaction
+            : {
+                ...transaction,
+                voted: true,
+                "likes-amount": transaction["likes-amount"] + 1
+              }
+      );
+      return {
+        ...state,
+        transactions: _transactions
+      };
+    }
+    case constants.UNLIKED_TRANSACTION: {
+      const trans = state.transactions.map(
+        transaction =>
+          transaction.id !== action.transactionId
+            ? transaction
+            : {
+                ...transaction,
+                voted: false,
+                "likes-amount": transaction["likes-amount"] - 1
+              }
+      );
+      return {
+        ...state,
+        transactions: trans
+      };
+    }
     case constants.RECEIVED_API_ERROR:
       return { ...state, fetching: false, error: action.error };
     default:
