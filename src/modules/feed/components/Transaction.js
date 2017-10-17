4330eaa377d58636bf2b4471220ecf3169d8b66b
bras1223 <l.hermans98@gmail.com>
2017-10-17 15:47:36 +0200

    Transaction only knows it's own properties and actions.

--- src/modules/feed/components/Transaction.js
+++ src/modules/feed/components/Transaction.js
@@ -3,32 +3,40 @@ import styles from "./Transaction.scss";
 import LikeIconInactive from "src/assets/icons/transaction/thumbs-up-inactive.svg";
 import LikeIconActive from "src/assets/icons/transaction/thumbs-up-active.svg";
 
-export const Transaction = ({ amount, from, to, reason, likes, liked }) => {
-  let likeButton;
-  if (liked) {
-    likeButton = <img src={LikeIconActive} class={styles.thumb} />;
+export const Transaction = ({ transaction, likeAction }) => {
+  let thumb;
+  if (transaction.voted) {
+    thumb = LikeIconActive;
   } else {
-    likeButton = <img src={LikeIconInactive} class={styles.thumb} />;
+    thumb = LikeIconInactive;
   }
-
   return (
     <div class={styles.transaction}>
       <div class={styles.transactionContent}>
         <div class={styles.transactionValue}>
-          <div class={styles.value}>{amount}</div>
+          <div class={styles.value}>
+            {transaction.amount + transaction["likes-amount"]}
+          </div>
           <div class={styles.kudoCurrency}>₭</div>
           <div class={styles.divider} />
         </div>
         <div class={styles.transactionDescription}>
           <div class={styles.transactionText}>
-            {from}: {amount} <span class={styles.kudoCurrency}>₭</span> to {to}{" "}
-            for {reason}
+            {transaction.sender.name}: {transaction.amount}{" "}
+            <span class={styles.kudoCurrency}>₭</span> to{" "}
+            {transaction.receiver.name} for {transaction.activity.name}
           </div>
           <div class={styles.transactionTimestamp}>1 day ago</div>
         </div>
         <div class={styles.transactionAction}>
-          {likeButton}
-          <div class={styles.likes}>+{likes}</div>
+          <a
+            onClick={() => {
+              likeAction(transaction.id);
+            }}
+          >
+            <img src={thumb} />
+          </a>
+          <div class={styles.likes}>+ {transaction["likes-amount"]}</div>
         </div>
       </div>
       <hr class={styles.hr} />
