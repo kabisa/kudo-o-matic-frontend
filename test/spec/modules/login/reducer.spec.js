2553b322a34bd6ff762e60d095d02967c619c904
bras1223 <l.hermans98@gmail.com>
2017-10-17 15:53:43 +0200

    Initial spec tests for Feed and Goal.

--- test/spec/modules/login/reducer.spec.js
+++ test/spec/modules/login/reducer.spec.js
@@ -1,12 +1,13 @@
 import { authentication } from "src/modules/login/reducer";
 import * as constants from "src/modules/login/constants";
+import Settings from "src/config/settings";
 
 describe("Authentication reducer", () => {
   it("returns the initial state", () => {
     expect(authentication(undefined, {})).to.eql({
       user: {
-        apiToken: undefined,
-        id: undefined,
+        apiToken: Settings.testApiToken,
+        id: Settings.testUserId,
         name: undefined,
         imageUri: undefined
       },
