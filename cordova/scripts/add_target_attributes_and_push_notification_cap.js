83a5b99987b9e4445afad4f5f7d5f005fe752370
Bart Vollebregt <bart@kabisa.nl>
2017-10-18 13:25:41 +0200

    Added hook to fix old project from Cordova BUILD_HOCKEY

--- cordova/scripts/add_target_attributes_and_push_notification_cap.js
+++ cordova/scripts/add_target_attributes_and_push_notification_cap.js
@@ -4,13 +4,13 @@ module.exports = function(context) {
   if(ROOT_DIR.substr(0, 1) === '/' && fs.existsSync(ROOT_DIR + "/platforms/ios")) {
 
 
-    var srcFile = ROOT_DIR + "/platforms/ios/My App.xcodeproj/project.pbxproj";
+    var srcFile = ROOT_DIR + "/platforms/ios/kudo-o-matic.xcodeproj/project.pbxproj";
 
     var projectPbxproj = fs.readFileSync(srcFile, "utf8");
 
     if(projectPbxproj.indexOf("TargetAttributes") === -1) {
       console.log("Adding TargetAttributes to pbxproj");
-      var targetAttributes = "\n\t\t\t\tTargetAttributes = {\n\t\t\t\t\t1D6058900D05DD3D006BFB54 = {\n\t\t\t\t\t\tDevelopmentTeam = F72EKUASP5;\n\t\t\t\t\t\tSystemCapabilities = {\n\t\t\t\t\t\t\tcom.apple.Push = {\n\t\t\t\t\t\t\t\tenabled = 1;\n\t\t\t\t\t\t\t};\n\t\t\t\t\t\t};\n\t\t\t\t\t};\n\t\t\t\t};";
+      var targetAttributes = "\n\t\t\t\tTargetAttributes = {\n\t\t\t\t\t1D6058900D05DD3D006BFB54 = {\n\t\t\t\t\t\tDevelopmentTeam = 9H6JXDUXPW;\n\t\t\t\t\t\tSystemCapabilities = {\n\t\t\t\t\t\t\tcom.apple.Push = {\n\t\t\t\t\t\t\t\tenabled = 1;\n\t\t\t\t\t\t\t};\n\t\t\t\t\t\t};\n\t\t\t\t\t};\n\t\t\t\t};";
       var searchString = "LastUpgradeCheck = 510;";
       var lastUpgradeCheckIndex = projectPbxproj.indexOf(searchString);
 
