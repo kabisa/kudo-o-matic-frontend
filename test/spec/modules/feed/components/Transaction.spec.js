fed70fbc4b600a0ec635f72f9dcce60ca60b9a2b
bras1223 <l.hermans98@gmail.com>
2017-10-30 17:02:04 +0100

    Fixed tests after adding functionality

--- test/spec/modules/feed/components/Transaction.spec.js
+++ test/spec/modules/feed/components/Transaction.spec.js
@@ -3,7 +3,7 @@ import Transaction from "src/modules/feed/components/Transaction";
 import styles from "src/modules/feed/components/Transaction.scss";
 import { shallow } from "preact-render-spy";
 
-describe("KudoCounter", function() {
+describe("Transaction Item", function() {
   let transaction;
 
   beforeEach(function() {
@@ -33,8 +33,9 @@ describe("KudoCounter", function() {
     expect(
       context.contains(
         <div class={styles.transactionText}>
-          Robin: 100 <span class={styles.kudoCurrency}>₭</span> to Luuk for
-          fixing bugs
+          Robin: 100 <span class={styles.kudoCurrency}>₭</span> to{" "}
+          <span id="receiver"> Luuk </span>
+          for <span id="activity">fixing bugs</span>
         </div>
       )
     ).to.be.true;
