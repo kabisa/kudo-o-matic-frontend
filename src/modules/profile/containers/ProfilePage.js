024dd77f35a45b46331f562aa0f3f7a1e486b110
bras1223 <l.hermans98@gmail.com>
2017-12-06 11:49:15 +0100

    Add Logout-functionality

--- src/modules/profile/containers/ProfilePage.js
+++ src/modules/profile/containers/ProfilePage.js
@@ -1,19 +1,38 @@
 import { h } from "preact";
+import { route } from "preact-router";
+import { connect } from "preact-redux";
 import { Page } from "src/components/Page";
 import { Header } from "src/components/Header";
 import I18n from "src/config/i18n";
+import styles from "./ProfilePage.scss";
+import { handleLogoutUser } from "../actions";
 
-const ProfilePage = ({ user }) => {
+const ProfilePage = ({ user, handleLogoutUser }) => {
+  const logOutUser = () => {
+    handleLogoutUser();
+    route("/login", true);
+  };
   return (
     <Page id="profilePage">
       <Header>
         <h1>{I18n.t("profile.title")}</h1>
       </Header>
-      <main>
-        <span>{user.name}</span>
+      <main class={styles.main}>
+        <h3 class={styles.name}>{user.name}</h3>
+        <img class={styles.profileImage} src={user.imageUri} />
+        <button className={styles.logoutButton} onClick={logOutUser}>
+          {I18n.t("profile.logout")}
+        </button>
       </main>
     </Page>
   );
 };
+const mapStateToProps = state => ({
+  user: state.authentication.user
+});
 
-export default ProfilePage;
+const mapDispatchToProps = {
+  handleLogoutUser
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
