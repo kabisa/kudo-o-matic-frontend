2f216528b9fcda50f191c80d65c17a2c05a31efa
bras1223 <l.hermans98@gmail.com>
2017-12-14 13:25:15 +0100

    Remove localStorage user at logout. Logout from Google.

--- src/modules/profile/actions.js
+++ src/modules/profile/actions.js
@@ -1,6 +1,10 @@
 import * as constants from "./constants";
+import { removeLogin } from "../../localStorage";
 
 export const handleLogoutUser = () => {
+  removeLogin();
+  window.plugins.googleplus.logout();
+
   return {
     type: constants.LOGOUT_USER
   };
