d1d04d27b8d29e7a9224f9fcb3519a4c087f1f4b
bras1223 <l.hermans98@gmail.com>
2017-12-14 09:28:01 +0100

    Add switch to open the right page at a notification click.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles/shell.scss";
 import { h, render } from "preact";
 import { Provider } from "preact-redux";
 import { Router as PreactRouter } from "preact-router";
+import { route } from "preact-router";
 import { history } from "src/support/history";
 import { augmentRouter } from "src/support/pageTransitionSupport";
 
@@ -30,6 +31,35 @@ const renderApp = function() {
 
 FastClick.attach(document.body);
 renderApp();
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+  window.FirebasePlugin.hasPermission(function(data) {
+    if (!data.isEnabled) {
+      window.FirebasePlugin.grantPermission();
+    }
+  });
+
+  window.FirebasePlugin.onNotificationOpen(
+    function(notification) {
+      switch (notification.gcm.event) {
+        case "transaction":
+          route("/feed", true);
+          break;
+        case "goal":
+          route("/goal", true);
+          break;
+        case "reminder":
+          route("/transaction", true);
+      }
+      console.log(notification);
+    },
+    function(error) {
+      console.error(error);
+    }
+  );
+}
 
 if (process.env.NODE_ENV !== "production") {
   require("preact/devtools");
