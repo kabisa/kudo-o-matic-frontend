8b77ee06607c3121c6544508d8209da4241d79ea
bras1223 <l.hermans98@gmail.com>
2017-12-20 14:23:14 +0100

    Using PullJS again, with some tweaks.

--- src/components/PullToRefresh.js
+++ src/components/PullToRefresh.js
@@ -1,8 +1,13 @@
 import { h, Component } from "preact";
-import Pull from "src/support/pulljs";
-import PullToRefresh from "pulltorefreshjs";
 import I18n from "src/config/i18n";
+import Pull from "src/support/pulljs";
+import iNoBounce from "inobounce";
 
+const baseSettings = {
+  ptrElement: "#pull-anchor",
+  mainElement: "#pull-container",
+  triggerElement: "#pull-container"
+};
 // !!!BIG HACK¡¡¡ to prevent having to re-init Pull all the time.
 // Basically we give it an anchor that does not get re-rendered,
 // so the library is free to inject code in it via DOM.
@@ -19,22 +24,28 @@ class PullAnchor extends Component {
 
 class PTR extends Component {
   componentDidMount() {
-    PullToRefresh.init({
-      mainElement: "#main",
-      onRefresh: function() {
-        alert("refresh");
-      }
-    });
-  }
+    const {
+      pullInstruction,
+      releaseInstruction,
+      refreshing,
+      onRefresh
+    } = this.props;
 
-  componentWillUnmount() {
-    const { handlers } = this.puller;
-    handlers && this.puller.destroy(handlers);
+    this.puller = Pull.init({
+      ...baseSettings,
+      instructionsPullToRefresh:
+        pullInstruction || I18n.t("pull_to_refresh.pull"),
+      instructionsReleaseToRefresh:
+        releaseInstruction || I18n.t("pull_to_refresh.release"),
+      instructionsRefreshing:
+        refreshing || I18n.t("pull_to_refresh.refreshing"),
+      onRefresh
+    });
   }
 
   render({ children, id, className, onScroll }) {
     return (
-      <main id="main" class={className} onScroll={e => onScroll(e)}>
+      <main id={id} class={className} onScroll={e => onScroll(e)}>
         <PullAnchor />
         <div id="pull-container">{children}</div>
       </main>
