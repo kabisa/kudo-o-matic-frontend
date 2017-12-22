0d44d3f33def77697d28e09d3fdd2ee268f32ff6
bras1223 <l.hermans98@gmail.com>
2017-12-22 11:16:44 +0100

    I18n processing.

--- src/modules/transaction/components/loadingScreen.js
+++ src/modules/transaction/components/loadingScreen.js
@@ -1,12 +1,13 @@
 import { h } from "preact";
 import styles from "./LoadingScreen.scss";
 import LoadingAnimation from "src/components/LoadingAnimation";
+import I18n from "src/config/i18n";
 
 const LoadingScreen = () => {
   return (
     <div class={styles.loadingScreen}>
       <LoadingAnimation />
-      <p>Processing transaction...</p>
+      <p>{I18n.t("transaction.processing")}</p>
     </div>
   );
 };
