0a4d71b5a8897680e885ebb5099c4ddc758f0b28
bras1223 <l.hermans98@gmail.com>
2017-12-27 17:02:46 +0100

    Add UserStatistics component to profilepage.

--- src/modules/profile/containers/ProfilePage.js
+++ src/modules/profile/containers/ProfilePage.js
@@ -3,6 +3,7 @@ import { route } from "preact-router";
 import { connect } from "preact-redux";
 import { Page } from "src/components/Page";
 import { Header } from "src/components/Header";
+import { UserStatistics } from "../components/UserStatistics";
 import I18n from "src/config/i18n";
 import styles from "./ProfilePage.scss";
 import { handleLogoutUser } from "../actions";
@@ -12,6 +13,10 @@ const ProfilePage = ({ user, handleLogoutUser }) => {
     handleLogoutUser();
     route("/login", true);
   };
+
+  user = {
+    name: "Luuk"
+  }
   return (
     <Page id="profilePage">
       <Header>
@@ -20,6 +25,8 @@ const ProfilePage = ({ user, handleLogoutUser }) => {
       <main class={styles.main}>
         <h3 class={styles.name}>{user.name}</h3>
         <img class={styles.profileImage} src={user.imageUri} />
+        <h3 class={styles.name}>Your transactions</h3>
+        <UserStatistics given={10} received={10} />
         <button className={styles.logoutButton} onClick={logOutUser}>
           {I18n.t("profile.logout")}
         </button>
