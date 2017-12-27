ca327f3bc9348ac0ae9e84abe605fe3851e95a39
bras1223 <l.hermans98@gmail.com>
2017-12-27 17:02:26 +0100

    Created a component that could display the user-stats.

---
+++ src/modules/profile/components/UserStatistics.js
@@ -0,0 +1,21 @@
+import { h } from "preact";
+import styles from "./UserStatistics.scss";
+
+export const UserStatistics = ({ given, received }) => {
+  return (
+    <div class={styles.userStatisticsContainer}>
+      <div class={styles.sendTransactions}>
+        <span class={styles.statisticsTitle}>Given</span>
+        <span class={styles.statisticsValue}>{given}</span>
+      </div>
+      <div class={styles.receivedTransactions}>
+        <span class={styles.statisticsTitle}>Received</span>
+        <span class={styles.statisticsValue}>{received}</span>
+      </div>
+      <div class={styles.totalTransactions}>
+        <span>Total</span>
+        <span class={styles.statisticsValue}>{given + received}</span>
+      </div>
+    </div>
+  );
+};
