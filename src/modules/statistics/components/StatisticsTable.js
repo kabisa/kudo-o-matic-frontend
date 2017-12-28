42087658b3c0fb6111bb136e9e4923146f69a3c2
bras1223 <l.hermans98@gmail.com>
2017-12-28 15:17:23 +0100

    Getting stats from in object, let component handle this instead of container.

--- src/modules/statistics/components/StatisticsTable.js
+++ src/modules/statistics/components/StatisticsTable.js
@@ -1,20 +1,20 @@
 import { h } from "preact";
 import styles from "./StatisticsTable.scss";
 
-export const StatisticsTable = ({ left, middle, right }) => {
+export const StatisticsTable = ({ stats }) => {
   return (
     <div class={styles.userStatisticsContainer}>
       <div class={styles.sendTransactions}>
-        <span class={styles.statisticsTitle}>{left.label}</span>
-        <span class={styles.statisticsValue}>{left.value}</span>
+        <span class={styles.statisticsTitle}>{stats.left.label}</span>
+        <span class={styles.statisticsValue}>{stats.left.value}</span>
       </div>
       <div class={styles.receivedTransactions}>
-        <span class={styles.statisticsTitle}>{middle.label}</span>
-        <span class={styles.statisticsValue}>{middle.value}</span>
+        <span class={styles.statisticsTitle}>{stats.middle.label}</span>
+        <span class={styles.statisticsValue}>{stats.middle.value}</span>
       </div>
       <div class={styles.totalTransactions}>
-        <span class={styles.statisticsTitle}>{right.label}</span>
-        <span class={styles.statisticsValue}>{right.value}</span>
+        <span class={styles.statisticsTitle}>{stats.right.label}</span>
+        <span class={styles.statisticsValue}>{stats.right.value}</span>
       </div>
     </div>
   );
