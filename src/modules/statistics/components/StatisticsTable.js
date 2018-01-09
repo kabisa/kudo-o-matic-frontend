467a8bc927ba841e8752159c83d347adb2bcedfe
bras1223 <l.hermans98@gmail.com>
2018-01-09 16:55:51 +0100

    Using i18n labels

--- src/modules/statistics/components/StatisticsTable.js
+++ src/modules/statistics/components/StatisticsTable.js
@@ -1,20 +1,21 @@
 import { h } from "preact";
+import I18n from "src/config/i18n";
 import styles from "./StatisticsTable.scss";
 
 export const StatisticsTable = ({ stats }) => {
   return (
     <div class={styles.userStatisticsContainer}>
       <div class={styles.sendTransactions}>
-        <span class={styles.statisticsTitle}>{stats.left.label}</span>
-        <span class={styles.statisticsValue}>{stats.left.value}</span>
+        <span class={styles.statisticsTitle}>{I18n.t("statistics.week")}</span>
+        <span class={styles.statisticsValue}>{stats.week}</span>
       </div>
       <div class={styles.receivedTransactions}>
-        <span class={styles.statisticsTitle}>{stats.middle.label}</span>
-        <span class={styles.statisticsValue}>{stats.middle.value}</span>
+        <span class={styles.statisticsTitle}>{I18n.t("statistics.month")}</span>
+        <span class={styles.statisticsValue}>{stats.month}</span>
       </div>
       <div class={styles.totalTransactions}>
-        <span class={styles.statisticsTitle}>{stats.right.label}</span>
-        <span class={styles.statisticsValue}>{stats.right.value}</span>
+        <span class={styles.statisticsTitle}>{I18n.t("statistics.total")}</span>
+        <span class={styles.statisticsValue}>{stats.total}</span>
       </div>
     </div>
   );
