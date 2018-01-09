f5e93fed7bf3beaebebc2d26de19d6349644abb3
bras1223 <l.hermans98@gmail.com>
2018-01-09 16:54:28 +0100

    Refactored UserStatistics component

--- src/modules/profile/components/UserStatistics.js
+++ src/modules/profile/components/UserStatistics.js
@@ -2,20 +2,20 @@ import { h } from "preact";
 import I18n from "src/config/i18n";
 import styles from "./UserStatistics.scss";
 
-export const UserStatistics = ({ given, received }) => {
+export const UserStatistics = ({ sent, received, total }) => {
   return (
     <div class={styles.userStatisticsContainer}>
-      <div class={styles.sendTransactions}>
-        <span class={styles.statisticsTitle}>{I18n.t("profile.given")}</span>
-        <span class={styles.statisticsValue}>{given}</span>
+      <div class={styles.leftStats}>
+        <span class={styles.statisticsTitle}>{I18n.t("profile.send")}</span>
+        <span class={styles.statisticsValue}>{sent}</span>
       </div>
-      <div class={styles.receivedTransactions}>
+      <div class={styles.leftStats}>
         <span class={styles.statisticsTitle}>{I18n.t("profile.received")}</span>
         <span class={styles.statisticsValue}>{received}</span>
       </div>
-      <div class={styles.totalTransactions}>
+      <div class={styles.rightStats}>
         <span class={styles.statisticsTitle}>{I18n.t("profile.total")}</span>
-        <span class={styles.statisticsValue}>{given + received}</span>
+        <span class={styles.statisticsValue}>{total}</span>
       </div>
     </div>
   );
