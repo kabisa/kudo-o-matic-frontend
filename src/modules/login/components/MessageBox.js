b2d581447335118c7ecc30b9497441cdcfbc7423
Jeroen van Thoor <j.vanthoor@student.fontys.nl>
2018-05-03 14:44:10 +0200

    Upgraded front-end and added translations BUILD_HOCKEY

--- src/modules/login/components/MessageBox.js
+++ src/modules/login/components/MessageBox.js
@@ -1,5 +1,6 @@
 import { h, Component } from "preact";
 import styles from "./MessageBox.scss";
+import I18n from "src/config/i18n";
 
 class MessageBox extends Component {
     constructor(props) {
@@ -12,9 +13,9 @@ class MessageBox extends Component {
         if (this.props.errorMessage != null || this.props.errorMessage != undefined) {
             switch (this.props.errorMessage) {
                 case "invalid_grant":
-                    return "No access";
+                    return I18n.t("messagebox.no_access");
                 case "missing_parameters":
-                    return "Username/password missing";
+                    return I18n.t("messagebox.missing_parameters");
                 default:
                     return this.props.errorMessage;
             }
