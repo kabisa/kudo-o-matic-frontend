d62213995e0cd3b3a545bd139739393fc81a875b
Jeroen van Thoor <j.vanthoor@student.fontys.nl>
2018-06-06 14:02:42 +0200

    Added the correct tests for the teamspage

---
+++ test/spec/modules/teams/actions.spec.js
@@ -0,0 +1,52 @@
+import * as actions from "src/modules/teams/actions";
+import * as constants from "src/modules/teams/constants";
+
+describe("Teams actions", () => {
+    it("finished fetching all teams", () => {
+        const teams = {
+            data: {
+                teams: [{
+                    id: 1,
+                    name: "NAME"
+                }, {
+                    id: 2,
+                    name: "NAME"
+                }],
+                invites: [{
+                    id: 3,
+                    name: "NAME"
+                }]
+            }
+        }
+        const expectedAction = {
+            type: constants.FINISHED_FETCHING_TEAMS,
+            teams: teams
+        };
+
+        expect(actions.finishedFetchingTeams(teams)).to.eql(expectedAction);
+    });
+
+    it("finished replying to invite", () => {
+        const API_TOKEN = "API_TOKEN";
+
+        const expectedAction = {
+            type: constants.FINISHED_REPLYING_TO_INVITE
+        };
+
+        expect(actions.finishedReplyingToInvite(API_TOKEN)).to.eql(expectedAction);
+    });
+
+    it("selected a team", () => {
+        const team = {
+            id: 1,
+            name: "NAME"
+        }
+
+        const expectedAction = {
+            type: constants.SELECTED_TEAM,
+            team: team
+        };
+
+        expect(actions.selectedTeam(team)).to.eql(expectedAction);
+    });
+});
