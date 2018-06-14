7db874fbe7c26a34fb6e1af67411e7b860b8cb52
Jeroen van Thoor <j.vanthoor@student.fontys.nl>
2018-06-14 10:55:22 +0200

    Fixed automated routing to the dashboard when only one team is available

--- src/modules/teams/actions.js
+++ src/modules/teams/actions.js
@@ -3,17 +3,13 @@ import { fetchTeams, replyInvite } from "src/modules/teams/apiClient";
 import { route } from "preact-router";
 
 export const finishedFetchingTeams = teams => {
-    if(teams.data.amountOfTeams == 1 && teams.data.amountOfInvites == 0) {
-        selectTeam(teams.data.teams[0].id);
-        route("/", true);        
-    }
     return {
         type: constants.FINISHED_FETCHING_TEAMS,
         teams: teams.data
     }
 }
 
-export const finishedReplyingToInvite = (apiToken) => {      
+export const finishedReplyingToInvite = (apiToken) => {
     return {
         type: constants.FINISHED_REPLYING_TO_INVITE
     }
@@ -32,16 +28,23 @@ export const selectedTeam = (team) => {
     }
 }
 
-export const selectTeam = (team) => { 
-    return dispatch => {        
+export const selectTeam = (team) => {
+    return dispatch => {
         dispatch(selectedTeam(team));
-    } 
+    }
 }
 
 export const fetchAllTeams = apiToken => {
     return dispatch => {
         return fetchTeams(apiToken)
-            .then(teams => dispatch(finishedFetchingTeams(teams)))
+            .then(teams => {
+                if (teams.data.amountOfTeams == 1 && teams.data.amountOfInvites == 0) {
+                    dispatch(selectedTeam(teams.data.teams[0]));
+                    route("/", true);
+                } else {
+                    dispatch(finishedFetchingTeams(teams))
+                }
+            })
             .catch(error => dispatch(receivedApiError(error)));
     }
 }
